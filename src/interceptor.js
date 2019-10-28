cc2b08746a6c7c0751f2f773bb4b7e6b4cf54be3
Nestor hdez <nestorhdez.26@gmail.com>
2019-10-28 12:56:32 +0000

    Home page done.

---
+++ src/interceptor.js
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import router from './router'
+
+const AuthAxios = axios.create();
+
+AuthAxios.interceptors.request.use((config) => {
+        const tokenObj = localStorage.getItem('quiz');
+        const token = JSON.parse(tokenObj).access_token
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    }, (error) => {
+        return error;
+  })
+
+AuthAxios.interceptors.response.use( (response) => {
+        // Return a successful response back to the calling service
+        return response;
+    }, (error) => {
+        // Return any error which is not due to authentication back to the calling service
+        if (error.response.status && error.response.status !== 401) {
+            return new Promise((resolve, reject) => {
+                reject(error);
+            });
+        }
+        if(error.response.status && error.response.status == 401 ){
+            const tokenObj = localStorage.getItem('quiz');
+            const token = JSON.parse(tokenObj)
+            return axios.post('http://localhost:3001/api/token', {refresh_token: token.refresh_token, grant_type: "refresh_token"})
+                .then(res => {
+                    localStorage.removeItem('quiz');
+                    localStorage.setItem('quiz', JSON.stringify(res.data));
+
+                    // New request with new token
+                    const config = error.config;
+                    config.headers.Authorization = `Bearer ${res.data.access_token}`;
+
+                    return axios(config);
+                })
+                .catch(function() {
+                    localStorage.removeItem('quiz');
+                    router.replace('/login');
+                })
+        }
+    });
+
+export default AuthAxios;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,65 @@
 <template>
     <div class="home">
         <h1>Quiz game</h1>
-        <Quiz :quiz="{title: '¿Esto es una pregunta?', answers: [{answer: 'esta', correct: true}, {answer: 'esta no', correct: false}]}"/>
-        <button>Next</button>
+        <span v-if="error.status">{{error.msg}}</span>
+        <span v-if="end" id="end">THE END</span>
+        <Quiz :quiz="questions[questionIndex]" v-if="questions.length > 0"/>
+        <button @click="next" v-if="!error.status && questions.length > 0">Next</button>
     </div>
 </template>
 
 <script>
 
 import Quiz from '../components/Quiz.vue';
+import Axios from '../interceptor';
 
 export default {
     name: 'home',
+    data() {
+        return {
+            questions: [],
+            questionIndex: 0,
+            error: {
+                status: false,
+                msg: ''
+            },
+            end: false
+        }
+    },
+    methods: {
+        getQuestions() {
+            this.error.status = false;
+            Axios.get(`${this.$url}/questions`)
+                .then(res => {
+                    if(res.data.res.length === 0) {
+                        this.error.status = true;
+                        this.error.msg = 'There are no questions to play...';
+                    }
+                    this.questions = res.data.res;
+                })
+                .catch(() => {
+                    this.error.status = true;
+                    this.error.msg = 'Something wrong happend...';
+                });
+        },
+        setQuestion() {
+            this.questionIndex = Math.floor((Math.random() * this.questions.length));
+        },
+        removeQuestion() {
+            this.questions = this.questions.filter((ques, i) => i != this.questionIndex);
+        },
+        next() {
+            this.removeQuestion();
+            this.setQuestion();
+            if(this.questions.length === 0) {
+                this.end = true;
+            }
+        }
+    },
+    created() {
+        this.getQuestions();
+        this.setQuestion();
+    },
     components: {
         Quiz
     }
@@ -22,6 +70,14 @@ export default {
     h1 {
         margin-bottom: 40px;
     }
+
+    span {
+		color: #c54040;
+        font-weight: 500;
+        font-size: 1.2rem;
+        margin-bottom: 20px;
+        display: inline-block;
+	}
 
     button {
         background-color: #2c3e50;
