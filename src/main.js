82226bc74504eb924db35d21372b11f60784dbb4
Nestor hdez <nestorhdez.26@gmail.com>
2019-10-28 11:30:03 +0000

    Log in page done.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Axios from 'axios'
 
-Vue.config.productionTip = false
+Vue.prototype.$axios = Axios;
+Vue.prototype.$url = 'http://localhost:3001/api';
+Vue.config.productionTip = false;
 
 new Vue({
   router,
